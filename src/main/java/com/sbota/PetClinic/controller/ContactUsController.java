834c128f1bdfc1d984cf927592d0769c008bd8b9
Bota Sorina <crisansorinageorgiana@yahoo.com>
2020-09-12 14:16:08 +0300

    contact us

---
+++ src/main/java/com/sbota/PetClinic/controller/ContactUsController.java
@@ -0,0 +1,14 @@
+package com.sbota.PetClinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContactUsController {
+
+    @GetMapping("/contactus")
+    public String loginPage(){
+
+        return "security/contactus";
+    }
+}
